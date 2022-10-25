05fe2561087c3465ad63e0d9f030ed1163f4f746
Ashir Imran <71806327+xlinkfrostx@users.noreply.github.com>
2022-10-25 15:54:38 -0400

    feedback form

--- components/Feedback.js
+++ components/Feedback.js
@@ -8,6 +8,10 @@ export default function Feedback() {
       <h1 className="font-bold text-4xl text-mmDarkBlue mb-6">Interested in MentorMatch? Let us know!</h1>
 
       <p className="text-xl text-mmDarkBlue max-w-4xl">If you are interested in MentorMatch, or have any other feedback to share on the idea, try out our prototype, and please fill out the form below!</p>
+
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf-wP3U0y4Tb1ySNndRonGlehu_EJL2BBOZIe1uq7ZGBUyhKQ/viewform?embedded=true" width="1000" height="1670" frameborder="0" marginheight="0" marginwidth="0">
+        Loading…
+      </iframe>
     </div>
   );
 }
